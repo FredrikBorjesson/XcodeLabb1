7d7c707046e7be9d7ef7f23d4c2c9cb67f69cbe2
fredrikborjesson <fredrik.m.borjesson@gmail.com>
2017-01-15 17:00:36 +0100

    Testing first commit and push

--- Labb1/AppDelegate.m
+++ Labb1/AppDelegate.m
@@ -2,7 +2,7 @@
 //  AppDelegate.m
 //  Labb1
 //
-//  Created by Fredrik Börjesson on 15/01/17.
+//  Created by Fredrik Börjesson on 12/01/17.
 //  Copyright © 2017 Fredrik Börjesson. All rights reserved.
 //
 
--- Labb1/ViewController.m
+++ Labb1/ViewController.m
@@ -2,17 +2,36 @@
 //  ViewController.m
 //  Labb1
 //
-//  Created by Fredrik Börjesson on 15/01/17.
+//  Created by Fredrik Börjesson on 12/01/17.
 //  Copyright © 2017 Fredrik Börjesson. All rights reserved.
 //
 
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UISlider *redSlider;
+@property (weak, nonatomic) IBOutlet UISlider *greenSlider;
+@property (weak, nonatomic) IBOutlet UISlider *blueSlider;
+@property (weak, nonatomic) IBOutlet UIView *colorView;
 
 @end
 
 @implementation ViewController
+
+-(void)refreshColorViev{
+    self.colorView.backgroundColor = [self currentColor];
+}
+
+-(UIColor*) currentColor{
+    return [UIColor colorWithRed:self.redSlider.value
+                           green:self.greenSlider.value
+                           blue:self.blueSlider.value
+                           alpha:1.0];
+}
+
+-(IBAction)sliderChanged:(UISlider *)sender{
+    [self refreshColorViev];
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
